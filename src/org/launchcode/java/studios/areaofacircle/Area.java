16ddd6dd1fc0c27c13bdcc1164ed7ad252bf1116
Nisha <rajendran.nisha@gmail.com>
2022-09-19 18:18:26 -0500

    Area of circle studio

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -6,7 +6,6 @@ public class Area {
 
     public static void main(String[] args) {
         Scanner input;
-
         input = new Scanner(System.in);
         System.out.println("Please enter the radius of the circle:");
 
